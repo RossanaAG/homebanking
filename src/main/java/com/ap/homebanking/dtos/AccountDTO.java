25f6645967be5dd811bf70eaa0c2e8e102d4dee9
Rossana <rossana.analia.gallardo@gmail.com>
2023-08-14 05:24:48 -0500

    Task 03

---
+++ src/main/java/com/ap/homebanking/dtos/AccountDTO.java
@@ -0,0 +1,50 @@
+package com.ap.homebanking.dtos;
+
+import com.ap.homebanking.models.Account;
+
+import java.time.LocalDateTime;
+
+public class AccountDTO {
+    private Long id;
+    private String number;
+    private LocalDateTime creationDate;
+    private double balance;
+
+    public AccountDTO(){
+
+    }
+    public AccountDTO(Account account) {
+        this.id = account.getId();
+        this.number = account.getNumber();
+        this.creationDate = account.getCreationDate();
+        this.balance = account.getBalance();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
